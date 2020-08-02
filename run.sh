383a59b4276c5c1d0236e161e21db6975251a896
psi <ryo.hirafuji@link-u.co.jp>
2020-08-02 16:12:34 +0900

    allow .fio extension

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ if [ $# -ne 2 ];then
     exit 1
 fi
 
-export FILENAME=$1
+export FILENAME="$1"
 fio --eta=always \
     --eta-newline=1s \
     --max-jobs=$(nproc) \
-    "$2.fio"
+    "$(basename "$2" .fio).fio"
