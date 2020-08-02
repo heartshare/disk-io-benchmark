ce5993d56bf52831eccb88ffc37f5ab60da8452a
psi <ryo.hirafuji@link-u.co.jp>
2020-08-02 16:02:50 +0900

    specify tempfile via args

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #! /bin/bash -eux
 
-fio --eta=always --eta-newline=1s --max-jobs=$(nproc) \
-    $1.fio
+if [ $# -ne 2 ];then
+    echo "Usage: bash run.sh <path-to-tmpfile> <jobname>";
+    exit 1
+fi
+
+export FILENAME=$1
+fio --eta=always \
+    --eta-newline=1s \
+    --max-jobs=$(nproc) \
+    "$2.fio"
